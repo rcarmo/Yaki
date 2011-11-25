0def36ba24286718af776a0d5db060ee6e02408e
Rui Carmo <rui.carmo@gmail.com>
2011-11-25 15:50:51 +0000

    added basic CLI args

--- yaki/app.py
+++ yaki/app.py
@@ -2,28 +2,58 @@
 
 import snakeserver.server
 import os, sys, time, hashlib
-
-HTTPD_PORT = 9090
-RUNAS_USER = None # "www"
-RUNAS_GROUP = None # "www"
+import getopt # we're not going to use argparse to maintain pre-2.7 compatibility
 
 # ---enable this to see possible memory leaks---
 #import gc
 #gc.set_debug(gc.DEBUG_LEAK)
 
-def main(workingdir=None):
+def usage():
+  print """Usage: %s [--debug][--bind=<ip>][--port=<num>][--external-port=<num>][--workdir=<path>]""" % sys.argv[1]
 
-  if workingdir:
-    os.chdir(workingdir)
+def main():
+  HTTPD_PORT = 9090
+  HTTPD_IP = '127.0.0.1'
+  HTTPD_EXTERNAL_PORT = 9090
+  RUNAS_USER = None # "www"
+  RUNAS_GROUP = None # "www"
+  WORKDIR = None
+  DEBUG = False
 
-  print >>sys.stderr,"%s GMT Starting server on port %d" % (time.asctime(time.gmtime()), HTTPD_PORT)
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["debug", "workdir=", "bind=", "port=", "external-port="])
+  except getopt.GetoptError, err:
+    print str(err)
+    usage()
+    sys.exit(2)
+  output = None
+  verbose = False
+  for o, a in opts:
+    if o in ("-b", "--bind"):
+      HTTPD_IP = a
+    elif o in ("-p", "--port"):
+      HTTPD_PORT = int(a)
+    elif o in ("-e", "--external-port"):
+      HTTPD_EXTERNAL_PORT = int(a)
+    elif o in ("-w", "--workdir"):
+      WORKDIR = a
+    elif o in ("-d", "--debug"):
+      DEBUG = True
+    else:
+      assert False, "unhandled option"
 
+  if WORKDIR:
+    os.chdir(WORKDIR)
+
+  print >>sys.stderr,"%s GMT Starting server on %s:%d (%d)" % (time.asctime(time.gmtime()), HTTPD_IP, HTTPD_PORT, HTTPD_EXTERNAL_PORT)
+
+  # TODO: move more of these to getopt above
   snakeserver.server.main(
     HTTPD_PORT=HTTPD_PORT,
-    bindname='127.0.0.1',
-    externalPort=80,
+    bindname=HTTPD_IP,
+    externalPort=HTTPD_EXTERNAL_PORT,
     serverURLprefix='',
-    debugRequests=False,
+    debugRequests=DEBUG,
     precompileYPages=True,
     writePageSource=False,
     serverRootDir=None,
@@ -34,4 +64,3 @@ def main(workingdir=None):
 
 if __name__=="__main__":
   main()
-
